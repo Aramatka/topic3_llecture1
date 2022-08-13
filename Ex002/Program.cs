f17ad6921c442f0be6ccae33e697fe9fee9174e8
Tamara <arepyevat@gmail.com>
2022-08-14 00:52:54 +0300

    2 Задача из лекции - нахождение  суммы

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 850 World!");
+﻿// Сложить  2 числа
+int numberA = 2;
+int numberB = 9;
+Console.Write("Сумма:");
+Console.WriteLine(numberA + numberB);

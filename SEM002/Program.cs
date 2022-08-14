c4a86c17bf4d95de85d31a4d7dba00220d9f8415
Tamara <arepyevat@gmail.com>
2022-08-15 00:22:47 +0300

    Проверка квадрата числа

--- SEM002/Program.cs
+++ SEM002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая на вход принимает два числа и проверяет является ли первое квадратом второго
+System.Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber == secondNumber*secondNumber)
+{
+    System.Console.WriteLine(firstNumber + " является к вадратом от " + secondNumber);
+}
+else
+{
+    System.Console.WriteLine(firstNumber + " не является к вадратом от " + secondNumber);
+}

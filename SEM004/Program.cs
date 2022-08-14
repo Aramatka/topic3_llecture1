4103e609b925c928437ef4750173cda9d0d5000b
Tamara <arepyevat@gmail.com>
2022-08-15 00:51:00 +0300

    Диапазон до числа

---
+++ SEM004/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход  число, на выход значения в промежутке от -Н до Н
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = number * (-1);
+
+}
+int negativeNumber = number * (-1);
+while (negativeNumber <= number)
+{
+    System.Console.Write(negativeNumber + ", ");
+    negativeNumber++;
+}

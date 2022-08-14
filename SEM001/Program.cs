c7b05df586cc21f2cc7970c9324d32787cc494c7
Tamara <arepyevat@gmail.com>
2022-08-15 00:05:10 +0300

    Первая задача семинара - квадрат числа 1

--- SEM001/Program.cs
+++ SEM001/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выдает квадрат числа
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine("результат " + square);

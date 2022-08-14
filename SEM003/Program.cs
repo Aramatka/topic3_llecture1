66be91f90bb17e89eba6235e04c8b61c4db4c6ea
Tamara <arepyevat@gmail.com>
2022-08-15 00:32:47 +0300

    Выведение дня недели

---
+++ SEM003/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывод дня недели по его номеру
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (dayNumber == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+if (dayNumber == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+if (dayNumber == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
